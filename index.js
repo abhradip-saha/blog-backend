a03e412bc207373f0fcf1aee73104ee5a21a1f62
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-02-03 18:23:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 // https://blogabhra.onrender.com
 // https://blogabhra.netlify.app
-app.use(cors({ credentials: true, origin: "https://blogabhra.onrender.com" }));
+app.use(cors({ credentials: true, origin: "https://blogabhra.netlify.app" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
