bb4448d154f276dc09be0af4971868365adf8962
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-02-05 11:03:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
-const uploadMiddleware = multer({ dest: "https://github.com/abhradip-saha/blog-backend/tree/main/uploads" });
+const uploadMiddleware = multer({ dest: "uploads" });
 const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
